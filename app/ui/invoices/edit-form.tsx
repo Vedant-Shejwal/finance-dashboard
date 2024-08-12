91f70ac2e62fbe2b88934fa7c88e1109949161d3
Vedant Shejwal <vedant.shejwal@mindstix.com>
2024-08-12 16:39:25 +0530

    implement server-side form validation

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,6 +1,7 @@
 'use client';
-import { updateInvoice } from '@/app/lib/actions';
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import { updateInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
 import {
   CheckIcon,
   ClockIcon,
@@ -17,9 +18,11 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-  return (
-    <form action={updateInvoiceWithId}>
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+    return (
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
